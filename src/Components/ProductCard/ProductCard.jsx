e82eefcf9dec6899b692d0b7a36ba084d4eca8dd
Torjus <Torjus24sfj@gmail.com>
2025-03-03 13:31:04 +0100

    styling buttons and text products

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,6 +1,6 @@
 import { Rating } from "@mui/material";
 
-const ProductCard = ({ name, description, price, stock, img }) => {
+const ProductCard = ({ name, description, price, stock, img, rating }) => {
     const handleAddToCart = () => {
 
     }
@@ -10,10 +10,10 @@ const ProductCard = ({ name, description, price, stock, img }) => {
             <img src={img} alt={`${name} image`} className="w-[240px]"/>
             <h3 className="font-bold text-[24px] mb-[5px]">{name}</h3>
             <p className="font-medium text-[14px]">{description}</p>
-            <div><Rating name="half-rating" defaultValue={0} precision={0.5} /></div> 
-            <p>{price}</p>
-            <p>{stock}</p>
-            <button onClick={handleAddToCart} disabled={stock === 0}>Add to Cart</button>
+            <div><Rating name="half-rating" defaultValue={rating} precision={0.5} readOnly /></div> 
+            <p>{price} kr</p>
+            <p className="text-[12px] text-gray-500 font-semibold">{stock} in stock</p>
+            <button onClick={handleAddToCart} disabled={stock === 0} className="border-2 border-gray-800 rounded-full p-2 text-[12px] mt-2 bg-gray-500 font-semibold hover:opacity-75">Add to Cart</button>
         </div>
         </>
     )
--- src/Components/ProductList/ProductList.jsx
+++ src/Components/ProductList/ProductList.jsx
@@ -1,40 +1,88 @@
 import ProductCard from "../ProductCard/ProductCard";
 
 const products = [
-    {
-        id: 1, name: 'Product-1', img: '/assets/placeholder_products.png', description: 'Description of product goes here', stock: 10, price: 0
-    },
-    {
-        id: 2, name: 'Product-2', img: '/assets/placeholder_products.png', description: 'Description of product goes here', stock: 10, price: 0
-    },
-    {
-        id: 3, name: 'Product-3', img: '/assets/placeholder_products.png', description: 'Description of product goes here', stock: 10, price: 0
-    },
-    {
-        id: 4, name: 'Product-4', img: '/assets/placeholder_products.png', description: 'Description of product goes here', stock: 10, price: 0
-    },
-    {
-        id: 5, name: 'Product-5', img: '/assets/placeholder_products.png', description: 'Description of product goes here', stock: 10, price: 0
-    },
-    {
-        id: 6, name: 'Product-6', img: '/assets/placeholder_products.png', description: 'Description of product goes here', stock: 10, price: 0
-    },
-    {
-        id: 7, name: 'Product-7', img: '/assets/placeholder_products.png', description: 'Description of product goes here', stock: 10, price: 0
-    },
-    {
-        id: 8, name: 'Product-8', img: '/assets/placeholder_products.png', description: 'Description of product goes here', stock: 10, price: 0
-    },
-]
+  {
+    id: 1,
+    name: "Product-1",
+    img: "/assets/placeholder_products.png",
+    description: "Description of product goes here",
+    stock: 10,
+    price: 1999,
+    rating: 2,
+  },
+  {
+    id: 2,
+    name: "Product-2",
+    img: "/assets/placeholder_products.png",
+    description: "Description of product goes here",
+    stock: 10,
+    price: 2499,
+    rating: 3.5,
+  },
+  {
+    id: 3,
+    name: "Product-3",
+    img: "/assets/placeholder_products.png",
+    description: "Description of product goes here",
+    stock: 10,
+    price: 599,
+    rating: 4,
+  },
+  {
+    id: 4,
+    name: "Product-4",
+    img: "/assets/placeholder_products.png",
+    description: "Description of product goes here",
+    stock: 10,
+    price: 12499,
+    rating: 5,
+  },
+  {
+    id: 5,
+    name: "Product-5",
+    img: "/assets/placeholder_products.png",
+    description: "Description of product goes here",
+    stock: 10,
+    price: 249,
+    rating: 3,
+  },
+  {
+    id: 6,
+    name: "Product-6",
+    img: "/assets/placeholder_products.png",
+    description: "Description of product goes here",
+    stock: 10,
+    price: 6999,
+    rating: 3.5,
+  },
+  {
+    id: 7,
+    name: "Product-7",
+    img: "/assets/placeholder_products.png",
+    description: "Description of product goes here",
+    stock: 10,
+    price: 34499,
+    rating: 5,
+  },
+  {
+    id: 8,
+    name: "Product-8",
+    img: "/assets/placeholder_products.png",
+    description: "Description of product goes here",
+    stock: 10,
+    price: 49,
+    rating: 4.5,
+  },
+];
 
 const ProductList = () => {
-    return (
-        <div className="product-list grid grid-cols-4 gap-4">
-        {products.map(product => (
-            <ProductCard key={product.id} {...product}/>
-        ))}
+  return (
+    <div className="product-list grid grid-cols-4 gap-4">
+      {products.map((product) => (
+        <ProductCard key={product.id} {...product} />
+      ))}
     </div>
-    )
-}
+  );
+};
 
 export default ProductList;
