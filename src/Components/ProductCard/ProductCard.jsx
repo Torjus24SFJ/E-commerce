a761df77edf0fc370649e7937cd7be6fe43de03c
Torjus <Torjus24sfj@gmail.com>
2025-03-03 13:19:40 +0100

    material ui rating component

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,4 +1,6 @@
-const ProductCard = ({ name, description, rating, price, stock, img }) => {
+import { Rating } from "@mui/material";
+
+const ProductCard = ({ name, description, price, stock, img }) => {
     const handleAddToCart = () => {
 
     }
@@ -6,9 +8,9 @@ const ProductCard = ({ name, description, rating, price, stock, img }) => {
         <>
         <div className="product-card p-[10px]">
             <img src={img} alt={`${name} image`} className="w-[240px]"/>
-            <h3 className="">{name}</h3>
-            <p>{description}</p>
-            <div>{rating}</div>
+            <h3 className="font-bold text-[24px] mb-[5px]">{name}</h3>
+            <p className="font-medium text-[14px]">{description}</p>
+            <div><Rating name="half-rating" defaultValue={0} precision={0.5} /></div> 
             <p>{price}</p>
             <p>{stock}</p>
             <button onClick={handleAddToCart} disabled={stock === 0}>Add to Cart</button>
