b6dcfe6261a7becb0948bf701eeeedef285eb94a
Torjus <Torjus24sfj@gmail.com>
2025-03-06 09:16:28 +0100

    handleCancelItem react icon

--- src/Components/Cartpage/Cartpage.jsx
+++ src/Components/Cartpage/Cartpage.jsx
@@ -1,13 +1,19 @@
 import { useNavigate } from "react-router-dom";
+import { MdCancel } from "react-icons/md";
 
 const CartPage = ({ checkout }) => {
-  const totalPrice = checkout.reduce(
-    (acc, item) => acc + item.price * item.amount, 0).toLocaleString();
+  const totalPrice = checkout
+    .reduce((acc, item) => acc + item.price * item.amount, 0)
+    .toLocaleString();
+
+  const navigation = useNavigate();
+  const handleCheckOut = () => {
+    navigation("/confirm");
+  };
+
+  const handleCancelItem = () => {
     
-    const navigation = useNavigate();
-    const handleCheckOut = () => {
-      navigation("/confirm")
-    }
+  }
 
   return (
     <section className="font-inter grid grid-rows-2 place-items-center text-black">
@@ -15,31 +21,45 @@ const CartPage = ({ checkout }) => {
         <h4 className="font-bold text-[26px]">Checkout</h4>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 p-6">
           {checkout.map((item, index) => (
-            <div key={item.id+index+item.name} className="w-100 grid grid-rows-[250px_1fr] justify-center text-center p-4 m-8">
+            <div
+              key={item.id + index + item.name}
+              className="w-100 grid grid-rows-[250px_1fr] justify-center text-center p-4 m-8"
+            >
               <img
                 src={item.img}
                 alt="product-image"
                 className="w-full h-full object-contain p-2"
               />
               <div className="">
-              <h4 className="font-semibold text-[16px]">{item.name}</h4>
-              <p className="text-[14px]"> <span className="font-bold">Quantity:</span> {item.amount}</p>
-              <p className="italic font-semibold text-[14px] text-[#64952f]">
-                {item.price} kr
-              </p>
+                <h4 className="font-semibold text-[16px]">{item.name}</h4>
+                <p className="text-[14px]">
+                  {" "}
+                  <span className="font-bold">Quantity:</span> {item.amount}
+                </p>
+                <p className="italic font-semibold text-[14px] text-[#64952f]">
+                  {item.price} kr
+                </p>
+                <div className="flex flex-row justify-end text-neutral-400 cursor-pointer">
+                  <MdCancel size={20} onClick={handleCancelItem}/>
+                </div>
               </div>
             </div>
           ))}
         </div>
         <div className="border-2 border-[#cbcbcb] bg-[#f1f1f1] p-8 mt-10">
-          <h4 className="mb-8 font-bold">Total: <span className="text-[#64952f]">{totalPrice}</span> kr</h4>
+          <h4 className="mb-8 font-bold">
+            Total: <span className="text-[#64952f]">{totalPrice}</span> kr
+          </h4>
           <p>Shipping</p>
           <p>Arrives Tue, Jul 20 - Thu, Jul 22</p>
           <p>Pickup</p>
           <p className="underline">Find a Store</p>
         </div>
         <div className="flex flex-col gap-2 mt-8">
-          <button className="bg-[#353535] text-[#f1f1f1] rounded-full p-2 font-bold cursor-pointer" onClick={handleCheckOut}>
+          <button
+            className="bg-[#353535] text-[#f1f1f1] rounded-full p-2 font-bold cursor-pointer"
+            onClick={handleCheckOut}
+          >
             Checkout
           </button>
         </div>
